1bf4dcae7a3a02d2f5e26309de4f8f92f9318237
hy1 <1337495759@qq.com>
2019-03-04 17:33:18 +0800

    博客项目

---
+++ Hblog/blog/urls.py
@@ -0,0 +1,29 @@
+from django.conf.urls import url
+
+from . import views
+
+
+urlpatterns = [
+
+    url(r'^index/$', views.index, name='index'),
+    url(r'^user_login/$', views.user_login, name='user_login'),
+    url(r'^user_logout/$', views.user_logout, name='user_logout'),
+    url(r'^register/$', views.register, name='register'),
+    url(r'^user_update/$', views.user_update, name='user_update'),
+    url(r'^user_list/$', views.user_list, name='user_list'),
+    url(r'^user_update/$', views.user_update, name='user_update'),
+    url(r'^article_add/$', views.article_add, name='article_add'),
+    url(r'^article_update/(?P<a_id>\d+)$', views.article_update, name='article_update'),
+    url(r'^article_delete/(?P<a_id>\d+)$', views.article_delete, name='article_delete'),
+    url(r'^article_detail/(?P<a_id>\d+)$', views.article_detail, name='article_detail'),
+    url(r'^article_list/$', views.article_list, name='article_list'),
+    url('^creataCode/$', views.creataCode, name='creataCode'),
+
+
+
+
+
+
+
+
+]
